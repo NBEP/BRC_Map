8b4c369d0cae8570f5c2e9d3ece0566ddc8cc36f
Mariel Sorlien <47087329+msorlien@users.noreply.github.com>
2023-03-21 14:15:02 -0400

    Update fun_brc_sites.R
    Added conductivity, water depth columns

--- data-raw/fun_brc_sites.R
+++ data-raw/fun_brc_sites.R
@@ -2,7 +2,7 @@
 #  TITLE: fun_brc_sites.R
 #  DESCRIPTION: Formats site data for BRC_Map R Shiny app
 #  AUTHOR(S): Mariel Sorlien
-#  DATE LAST UPDATED: 2023-03-20
+#  DATE LAST UPDATED: 2023-03-21
 #  GIT REPO:
 #  R version 4.2.0 (2022-04-22 ucrt) x86_64
 ##############################################################################.
@@ -25,8 +25,11 @@ brcmap_format_sites <- function(sites_db, output_path){
                              !(TOWN %in% RI_towns) ~ 'MA')) %>%
     # Select columns
     select(SITE_NUMBER, BRC_CODE, SITE_NAME, LATITUDE, LONGITUDE,
-           WATERBODY_NAME, ZONE, CFR, TOWN, STATE, HUC12_NUM, HUC_NAME)
+           WATERBODY_NAME, ZONE, CFR, TOWN, STATE, HUC12_NUM, HUC_NAME,
+           CONDUCTIVITY_USCM, WATER_DEPTH_FT)
 
   # Save as rds
   save(brc_sites, file=file.path(output_path, 'brc_sites.rda'))
 }
+
+brcmap_format_sites('data-raw/sites_db.rds', 'data')
