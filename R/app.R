59150613fe7bab6cb75e9699b27ea3f33f4d3162
Mariel Sorlien <47087329+msorlien@users.noreply.github.com>
2023-03-27 11:20:08 -0400

    Update app.R
    Renamed variables, moved dependencies to description

--- R/app.R
+++ R/app.R
@@ -2,37 +2,12 @@
 #  TITLE: app.R
 #  DESCRIPTION: R shiny app for displaying BRC water quality data
 #  AUTHOR(S): Mariel Sorlien
-#  DATE LAST UPDATED: 2023-03-15
+#  DATE LAST UPDATED: 2023-03-27
 #  GIT REPO:
 #  R version 4.2.0 (2022-04-22 ucrt)  x86_64
 ##############################################################################.
 
-library(shiny)
-library(tidyverse)
-library(bslib)
-
-# For testing purposes
-library(shinya11y)
-
-blackstonemap <- function(...){
-  # Data ----
-  sites_db <- sites_db %>%
-    mutate(SITE_NAME = make.unique(SITE_NAME, sep=" ")) %>%
-    mutate(FISHERY = case_when(CFR=='Yes' ~ 'Coldwater',
-                               CFR=='No' ~ 'Warmwater'))
-
-  parameters_db <- parameters_db %>%
-    filter(!is.na(AVG) & !is.na(AVG_CWF) & !is.na(EXC) & !is.na(GOOD))
-
-  data_db <- data_db %>%
-    # Remove null values
-    filter(RESULT != -999999) %>%
-    # Set DATE_TIME to datetime format
-    mutate(DATE_TIME = ymd_hm(DATE_TIME, tz = "America/New_York")) %>%
-    # Calc year
-    mutate(YEAR = lubridate::year(DATE_TIME)) %>%
-    # Rename columns
-    rename(BRC_CODE = SITE_BRC_CODE)
+BRC_Map <- function(...){
 
   ########################################################################.
   #                         User Interface                               #
@@ -40,7 +15,7 @@ blackstonemap <- function(...){
 
   ui <- fluidPage(
     #theme = bslib::bs_theme(bootswatch = 'sandstone'),
-    use_tota11y(),
+    #use_tota11y(),
 
     tags$header(
       class = "col-sm-12 title-panel",
@@ -51,7 +26,7 @@ blackstonemap <- function(...){
       # * Side panel ----
       sidebarPanel(
         # Module: select variables
-        BRCVAR_UI('brcvar', data_db, data_score_db)
+        BRCVAR_UI('brcvar', brc_data_num, brc_data_text, brc_data_score)
 
       ),
 
@@ -96,12 +71,12 @@ blackstonemap <- function(...){
   ########################################################################.
   server <- function(input, output, session) {
     # Add module servers ----
-    brcvar <- BRCVAR_SERVER('brcvar', sites_db, parameters_db, data_db,
-                            data_score_db, reactive({ input$tabset }))
+    brcvar <- BRCVAR_SERVER('brcvar', brc_sites, brc_parameters, brc_data_num,
+                            brc_data_score, reactive({ input$tabset }))
     BRCMAP_SERVER('brcmap', brcvar)
     REPORTCARD_SERVER('reportCard', brcvar)
-    BRCGRAPHS_SERVER('brcgraphs', sites_db, parameters_db, brcvar)
-    BRCDOWNLOAD_SERVER('brcdownload', brcvar)
+    BRCGRAPHS_SERVER('brcgraphs', brc_sites, brc_parameters, brcvar)
+    BRCDOWNLOAD_SERVER('brcdownload', brc_data_text, brcvar)
   }
 
 
